7890a7418bb75dcbc0ce510e8b47d604a18486da
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-05-12 22:18:30 +0300

    Initialized Spring application and connected the DB

---
+++ implementation/advDB/src/main/java/com/nuggets/advDB/CarServiceCenterApplication.java
@@ -0,0 +1,13 @@
+package com.nuggets.advDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceCenterApplication.class, args);
+	}
+
+}
---
+++ implementation/advDB/src/test/java/com/nuggets/advDB/CarServiceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuggets.advDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
