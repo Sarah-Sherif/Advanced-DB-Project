013dd82f5ad8d98387885a02fdd1af164c45c6ec
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-05-15 14:46:37 +0300

    Initialized second application

---
+++ implementation/carServiceCenter/src/main/java/com/advDB/carServiceCenter/CarServiceCenterApplication.java
@@ -0,0 +1,13 @@
+package com.advDB.carServiceCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceCenterApplication.class, args);
+	}
+
+}
---
+++ implementation/carServiceCenter/src/test/java/com/advDB/carServiceCenter/CarServiceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advDB.carServiceCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
