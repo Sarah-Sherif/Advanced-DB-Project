275e287d0ec378d597e975d04972de5214748163
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-05-16 04:30:27 +0300

    Cors Error

--- implementation/carServiceCenter/src/main/java/com/advDB/carServiceCenter/CarServiceCenterApplication.java
+++ implementation/carServiceCenter/src/main/java/com/advDB/carServiceCenter/CarServiceCenterApplication.java
@@ -2,10 +2,31 @@ package com.advDB.carServiceCenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CarServiceCenterApplication {
 
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		//config.setAllowCredentials(true); // you USUALLY want this
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CarServiceCenterApplication.class, args);
 	}
--- implementation/carServiceCenter/src/main/java/com/advDB/carServiceCenter/resource/rest/CarResource.java
+++ implementation/carServiceCenter/src/main/java/com/advDB/carServiceCenter/resource/rest/CarResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/car")
+@CrossOrigin(origins = "http://localhost:8081")
 public class CarResource {
 
     @Autowired
